dca9501190110c232f57b330985d63776d2faf9c
Guilherme João Correia Lopes <fc52761@alunos.fc.ul.pt>
2021-11-23 01:37:44 +0000

    MapFragment improved, small fixes and main Fragments TODO: Start building Fragments for Prototype

--- app/src/main/java/com/example/cityquest/activities/SplashActivity.java
+++ app/src/main/java/com/example/cityquest/activities/SplashActivity.java
@@ -1,10 +1,14 @@
-package com.example.cityquest;
+package com.example.cityquest.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.WindowManager;
+
+import com.example.cityquest.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +16,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
 
         if(getSupportActionBar() != null) getSupportActionBar().hide();
         new CountDownTimer(1000, 1000)  { //change back to 3000, 1000 is for testing purposes
